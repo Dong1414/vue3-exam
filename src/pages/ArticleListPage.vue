04eb553e9ad3c14856a0a5bdff7eec3865b7d28f
Dong1414 <homar1@naver.com>
2021-02-22 21:07:46 +0900

    TitleBar 전역 컴포넌트

--- src/pages/ArticleListPage.vue
+++ src/pages/ArticleListPage.vue
@@ -4,11 +4,11 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import TitleBar from '../components/TitleBar.vue'
+
 export default defineComponent({
   name: 'ArticleListPage',
   components: {
-      TitleBar
+
   }
 })
 </script>
