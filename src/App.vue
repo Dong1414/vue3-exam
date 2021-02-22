8a431c15f5992cc66e969ee9d5a301e4acf827cc
Dong1414 <homar1@naver.com>
2021-02-22 19:46:58 +0900

    필요없는 파일 제거

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id nemo earum, enim ad iste veritatis at corporis incidunt consequatur laudantium sit, magni sequi fuga fugit voluptatibus porro repellat tempora exercitationem?
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+})
+</script>
+
+<style>
+html, body{
+  margin:0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  
+}
+</style>
