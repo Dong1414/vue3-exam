5a9c87eca70b04458e9ffc78f23690e2f20f5fb0
Dong1414 <70949409+Dong1414@users.noreply.github.com>
2021-02-28 17:55:37 +0900

    백엔드 게시물 리스트에 노출

--- src/pages/ArticleListPage.vue
+++ src/pages/ArticleListPage.vue
@@ -63,23 +63,27 @@
 import { defineComponent, ref, reactive, getCurrentInstance, onMounted } from 'vue'
 import { IArticle } from '../types/'
 import { MainApi } from '../apis/'
-import { Article } from '../dtos/'
+import { useRoute } from 'vue-router'
 
 export default defineComponent({
   name: 'ArticleListPage',
    setup() {
+      const route = useRoute();
        const mainApi:MainApi = getCurrentInstance()?.appContext.config.globalProperties.$mainApi;
-    function loadArticles() {
-      mainApi.article_list(1)
-      .then(axiosResponse => {
-        console.log(axiosResponse.data.body.articles);
-      });
-    }
-    onMounted(loadArticles);
+    
     const newArticleTitleElRef = ref<HTMLInputElement>();
     const newArticleBodyElRef = ref<HTMLInputElement>();
     const state = reactive({
-      articles: [] as Article[]
+        articles: [] as IArticle[]
+    });
+    function loadArticles(boardId:number) {
+      mainApi.article_list(boardId)
+      .then(axiosResponse => {
+        state.articles = axiosResponse.data.body.articles;
+      });
+    }
+    onMounted(() => {
+      loadArticles(1);
     });
     function checkAndWriteArticle() {
       if ( newArticleTitleElRef.value == null ) {
@@ -108,8 +112,7 @@ export default defineComponent({
       newArticleTitleEl.focus();
     }
     function writeArtile(title:string, body:string) {
-      const newArticle = new Article(title, body);
-      state.articles.push(newArticle);
+      
     }
     return {
       state,
