6d939bebb75c10926205fee47428fc643b9d708e
Dong1414 <70949409+Dong1414@users.noreply.github.com>
2021-03-01 15:49:17 +0900

    상세페이지 추가, 디자인 변경, 메뉴바 변경, 로그인 없을 시 글 추가 실패 후 로그인 창으로

--- src/pages/ArticleWritePage.vue
+++ src/pages/ArticleWritePage.vue
@@ -3,25 +3,33 @@
 
   <section class="section section-article-write-form px-2">
     <div class="container mx-auto">
-      <form v-on:submit.prevent="checkAndWriteArticle">
-        <FormRow title="제목">
-          <input ref="newArticleTitleElRef" class="form-row-input" type="text" placeholder="제목을 입력해주세요.">
-        </FormRow>
-        <FormRow title="내용">
-          <textarea ref="newArticleBodyElRef" class="form-row-input" placeholder="내용을 입력해주세요."></textarea>
-        </FormRow>
-        <FormRow title="작성">
-          <div class="btns">
-            <input type="submit" value="작성" class="btn-primary" />
-          </div>
-        </FormRow>
-      </form>
+      <div class="px-6 py-6 bg-white rounded-lg shadow-md">
+        <form v-on:submit.prevent="checkAndWriteArticle">
+          <FormRow title="게시판">
+            <select class="form-row-select" ref="newArticleBoardIdElRef">
+              <option value="1">공지사항</option>
+              <option value="2">자유</option>
+            </select>
+          </FormRow>
+          <FormRow title="제목">
+            <input ref="newArticleTitleElRef" class="form-row-input" type="text" placeholder="제목을 입력해주세요.">
+          </FormRow>
+          <FormRow title="내용">
+            <textarea ref="newArticleBodyElRef" class="form-row-input" placeholder="내용을 입력해주세요."></textarea>
+          </FormRow>
+          <FormRow title="작성">
+            <div class="btns">
+              <input type="submit" value="작성" class="btn-primary" />
+            </div>
+          </FormRow>
+        </form>
+      </div>
     </div>
   </section>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, reactive, getCurrentInstance, onMounted, watch } from 'vue'
+import { defineComponent, ref, reactive, getCurrentInstance, onMounted } from 'vue'
 import { IArticle } from '../types/'
 import { MainApi } from '../apis/'
 import { Router } from 'vue-router';
@@ -37,12 +45,28 @@ export default defineComponent({
   setup(props) {
     const router:Router = getCurrentInstance()?.appContext.config.globalProperties.$router;
     const mainApi:MainApi = getCurrentInstance()?.appContext.config.globalProperties.$mainApi;
+
+    const newArticleBoardIdElRef = ref<HTMLInputElement>();
     const newArticleTitleElRef = ref<HTMLInputElement>();
     const newArticleBodyElRef = ref<HTMLInputElement>();
+
+     onMounted(() => {
+      if ( newArticleBoardIdElRef.value == null ) {
+        return;
+      }
+      
+      newArticleBoardIdElRef.value.value = props.boardId + "";
+    })
+    
     const state = reactive({
       articles: [] as IArticle[]
     });
     function checkAndWriteArticle() {
+      if ( newArticleBoardIdElRef.value == null ) {
+        return;
+      }
+      const newArticleBoardIdEl = newArticleBoardIdElRef.value;
+
       if ( newArticleTitleElRef.value == null ) {
         return;
       }
@@ -63,22 +87,23 @@ export default defineComponent({
         newArticleBodyEl.focus();
         return;
       }
-      writeArtile(newArticleTitleEl.value, newArticleBodyEl.value);
+      writeArtile(parseInt(newArticleBoardIdEl.value), newArticleTitleEl.value, newArticleBodyEl.value);
       newArticleTitleEl.value = '';
       newArticleBodyEl.value = '';
       newArticleTitleEl.focus();
     }
-    function writeArtile(title:string, body:string) {
-      mainApi.article_doWrite(props.boardId, title, body)
+     function writeArtile(boardId:number, title:string, body:string) {
+      mainApi.article_doWrite(boardId, title, body)
         .then(axiosResponse => {
           const newArticleId = axiosResponse.data.body.id;
           alert(newArticleId + "번 글이 생성되었습니다.");
-          router.push("/detail?id=" + newArticleId);
+           router.push("detail?id=" + newArticleId);
         });
     }
     return {
       state,
       checkAndWriteArticle,
+      newArticleBoardIdElRef,
       newArticleTitleElRef,
       newArticleBodyElRef
     }
